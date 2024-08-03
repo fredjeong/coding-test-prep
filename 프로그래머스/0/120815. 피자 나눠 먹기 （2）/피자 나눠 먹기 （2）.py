2ebe817a2cfeb45969d173c71634a5ced1d3b0a6
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-03 23:45:16 +0100

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,14 +1,5 @@
 def solution(n):
-    if n % 6 == 0:
-        answer = n // 6
-    elif n % 6 == 1:
-        answer = n
-    elif n % 6 == 2:
-        answer = n // 6 * 3 + 1
-    elif n % 6 == 3: 
-        answer = n // 3
-    elif n % 6 == 4: 
-        answer = n // 6 * 3 + 2
-    elif n % 6 == 5: 
-        answer = n
+    answer = 1
+    while 6 * answer % n != 0:
+        answer += 1
     return answer
