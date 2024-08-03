16c237fa9cffb556a9c39190865f89fd9d1eb8ab
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-03 22:55:48 +0100

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,9 +1,6 @@
 def solution(numbers, direction):
-    answer = []
     if direction == 'right':
-        answer.append(numbers[-1])
-        answer += numbers[:-1]
+        answer = [numbers[-1]] + numbers[:-1]
     else:
-        answer += numbers[1:]
-        answer.append(numbers[0])
+        answer = numbers[1:] + [numbers[0]]
     return answer
