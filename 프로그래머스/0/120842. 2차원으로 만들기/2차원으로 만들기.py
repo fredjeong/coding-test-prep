446722c3c6815943ab3b8d048ba522b0d822c7f2
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-04 00:22:23 +0100

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,7 +1,5 @@
 def solution(num_list, n):
     answer = []
-    key = len(num_list) // n
-    for i in range(1,key+1):
-        answer += [num_list[:n:]]
-        num_list = num_list[-n*(key-i):]
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i+n])
     return answer
