0e5775ce7cdc9d1bbc0b515b919d5b2e4b0dcabb
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-08 23:56:46 +0100

    [level 0] Title: 원하는 문자열 찾기, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -0,0 +1,10 @@
+def solution(myString, pat):
+    myString = myString.lower()
+    pat = pat.lower()
+    
+    if pat in myString:
+        answer = 1
+    else:
+        answer = 0
+    
+    return answer
