1b5687627aa4c6faedea6b79adec5d6e3e67886f
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-10 05:56:11 +0100

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 683.72 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -1,20 +1,17 @@
 def solution(arr):
-    arr_past = [] + arr
     answer = 0
-    arr_new = [] + arr_past
-    for i in range(len(arr_new)):
-        if arr_new[i] >= 50 and arr_new[i] % 2 == 0:
-            arr_new[i] = arr_new[i] // 2
-        elif arr_new[i] < 50 and arr_new[i] % 2 == 1:
-            arr_new[i] = arr_new[i] * 2 + 1
-                
-    while arr_past != arr_new:
-        answer += 1
-        arr_past = [] + arr_new
-        for i in range(len(arr_new)):
-            if arr_new[i] >= 50 and arr_new[i] % 2 == 0:
-                arr_new[i] /= 2
-            elif arr_new[i] < 50 and arr_new[i] % 2 == 1:
-                arr_new[i] = arr_new[i] * 2 + 1
-
+    arr_past = arr
+    while True:
+        arr_new = []
+        for i in arr_past:
+            if i >= 50 and i % 2 == 0:
+                i = i // 2
+            elif i < 50 and i % 2 == 1:
+                i = i * 2 + 1
+            arr_new.append(i)
+        if arr_new == arr_past:
+            break
+        else:
+            arr_past = arr_new
+            answer += 1
     return answer
