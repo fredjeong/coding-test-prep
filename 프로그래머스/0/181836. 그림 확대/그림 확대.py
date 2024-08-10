a8b3e314c0701b3af0e20cf4bacf610c06c3d2a1
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-10 20:39:33 +0100

    [level 0] Title: 그림 확대, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -1,13 +1,6 @@
 def solution(picture, k):
-    original = [[picture[row][col] for col in range(len(picture[0]))] for row in range(len(picture))]
-    new = [["A" for _ in range(len(picture[0] * k))] for _ in range(len(picture) * k)]
+    answer = []
     for i in range(len(picture)):
-        for j in range(len(picture[0])):
-            for row in range(k):
-                for col in range(k):
-                    new[i*k+row][j*k + col] = original[i][j]
-    answer = ['' for _ in range(len(picture)*k)]
-    for i in range(len(new)):
-        for j in range(len(new[0])):
-            answer[i] += new[i][j]
+        for _ in range(k):
+            answer.append(picture[i].replace('.', '.' * k).replace('x', 'x' * k))
     return answer
