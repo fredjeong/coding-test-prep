efbfbf13f9cbbaefb0891a1643be0ffafc982e9f
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-21 18:40:38 +0100

    [level 2] Title: 전화번호 목록, Time: 468.87 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -5,14 +5,11 @@ def solution(phone_book):
     for nums in phone_book: 
         dic[nums] = 1 
     
-    # 2.접두어가 Hash map에 존재하는지 찾기 
-    for nums in phone_book: 
+    for number in phone_book: 
         arr = "" 
-        for num in nums: 
+        for num in number: 
             arr += num
-    
-            # 3. 본인 자체일 경우는 제외
-            if arr in dic and arr != nums:       
+            if arr in dic and arr != number:       
                 answer = False
                 break
                 
