44ed6b9033fcfe3105c32f8c14bdba6bda76675d
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-23 17:30:18 +0100

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,8 +1,2 @@
-select animal_id, animal_type, name
-from animal_ins
-where (animal_id in (select animal_id from animal_outs where sex_upon_outcome like 'Spayed%' or sex_upon_outcome like 'Neutered%')) and sex_upon_intake like "Intact%"
-
-#select a.animal_id, a.animal_type, a.name
-#from animal_ins a join animal_outs b on a.animal_id = b.animal_id
-#where a.sex_upon_intake != b.sex_upon_outcome
-#order by a.animal_id asc
+-- 코드를 입력하세요
+SELECT
