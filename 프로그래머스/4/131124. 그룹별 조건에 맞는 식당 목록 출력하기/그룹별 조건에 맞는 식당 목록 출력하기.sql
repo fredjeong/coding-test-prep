19206ad2db88bd4aa90ad1fcada2fa28d2452f97
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-25 15:11:49 +0100

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,29 +1,13 @@
 select a.member_name, b.review_text, date_format(b.review_date, '%Y-%m-%d') as review_date
 from member_profile a join rest_review b on a.member_id = b.member_id
-where a.member_id in (select member_id#, counting
-from(
-select member_id, count(member_id) as counting
-from rest_review
-group by member_id
-order by count(member_id) desc) as adsf
-where counting in (select max(counting) as max_count
-from (select count(member_id) as counting from rest_review group by member_id order by count(member_id) desc) as aefq))
+where a.member_id in (select member_id
+                      from(select member_id, count(member_id) as counting
+                           from rest_review
+                           group by member_id
+                           order by count(member_id) desc) as temp_1
+                      where counting in (select max(counting) as max_count
+                                         from (select count(member_id) as counting 
+                                               from rest_review 
+                                               group by member_id 
+                                               order by count(member_id) desc) as temp_2))
 order by b.review_date asc, b.review_text asc
-##order by a.member_name
-##group by a.member_name
-#
-##select member_id
-##where member_id 
-#
-
-
-#select member_id#, counting
-#from(
-#select member_id, count(member_id) as counting
-#from rest_review
-#group by member_id
-#order by count(member_id) desc) as adsf
-#where counting in (select max(counting) as max_count
-#from (select count(member_id) as counting from rest_review group by member_id order by count(member_id) desc) as aefq)
-
-
