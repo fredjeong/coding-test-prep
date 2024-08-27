4b7876acc7ed8726b5a0dd8fbbfd6ed9e7166c5e
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-27 14:51:39 +0100

    [Bronze V] Title: 개, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.py
@@ -0,0 +1,11 @@
+line_1 = "|\\_/|"
+line_2 = "|q p|   /}"
+line_3 = '( 0 )"""\\'
+line_4 = '|"^"`    |'
+line_5 = "||_/=\\\\__|"
+
+print(line_1)
+print(line_2)
+print(line_3)
+print(line_4)
+print(line_5)
