e314bcb59390cdabb2d3ebde704d3da10603e052
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-08-31 16:39:29 +0100

    [level 2] Title: 2개 이하로 다른 비트, Time: 213.35 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,32 +1,37 @@
 def solution(numbers):
     answer = []
-    
     for number in numbers:
-        bin_number = list('0' + bin(number)[2:])
-        idx = ''.join(bin_number).rfind('0')
-        bin_number[idx] = '1'
-        
-        if number % 2 == 1:
-            bin_number[idx+1] = '0'
-        
-        answer.append(int(''.join(bin_number), 2))
-    
+        binary = "0" * (17 - len(bin(number)[2:])) + bin(number)[2:]
+        while True:
+            counter = 0
+            number += 1
+            new_binary = "0" * (17 - len(bin(number)[2:])) + bin(number)[2:]
+            for i in reversed(range(len(binary))):
+                if new_binary[i] != binary[i]:
+                    counter += 1
+            if counter > 2:
+                continue
+            answer.append(int('0b' + str(int(new_binary)), 2))
+            break
     return answer
-
-# 시간 초과
-#def solution(numbers):
-#    answer = []
+                
+        
+    
+    
+#    # numbers 안의 각 수에 대해서
 #    for number in numbers:
-#        binary = "0" * (17 - len(bin(number)[2:])) + bin(number)[2:]
+#        # 1의 개수 세기
+#        # 그보다 큰 수 중 비트가 다른 지점이 2개 이하인 수 추출
 #        while True:
-#            counter = 0
-#            number += 1
-#            new_binary = "0" * (17 - len(bin(number)[2:])) + bin(number)[2:]
-#            for i in reversed(range(len(binary))):
-#                if new_binary[i] != binary[i]:
-#                    counter += 1
-#            if counter > 2:
-#                continue
-#            answer.append(int('0b' + str(int(new_binary)), 2))
-#            break
+#            number +=1 
+#            # 1의 개숙 
+#            
+#            
+#            answer.append()
 #    return answer
+
+            
+            
+        
+    
+    
--- 프로그래머스/2/77885. 2개 이하로 다른 비트/README.md
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/README.md
@@ -1,6 +1,6 @@
 # [level 2] 2개 이하로 다른 비트 - 77885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77885#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 31일 16:38:57
+2024년 08월 31일 16:39:27
 
 ### 문제 설명
 
