987713a82b10aa156a109fb3d95b800f31916ff8
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-09-02 22:00:17 +0100

    [level 2] Title: 메뉴 리뉴얼, Time: 95.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,19 +1,9 @@
 from itertools import combinations
 
-def solution(orders, course):
-    course_num_arr = []
-    for order in orders:
-        if len(order) not in course_num_arr:
-            course_num_arr.append(len(order))
-    
+def solution(orders, course):    
     answer = []
-    max_num_dishes = 0
-    for order in orders:
-        if len(order) > max_num_dishes:
-            max_num_dishes = len(order)
-    course_num = 2
+
     for course_num in course:
-    #while course_num <= max_num_dishes:
         course = {}
         for order in orders:
             comb = list(combinations(sorted(order), course_num))
@@ -26,7 +16,6 @@ def solution(orders, course):
         for i in course:
             if course[i] == max(course.values()) and max(course.values()) > 1:
                 answer.append(i)
-        #course_num += 1
 
     answer = sorted(answer)
     return answer
