731c6802c4fa74ea28b3a0575a4203be417963b8
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-09-04 01:33:36 +0100

    [level 2] Title: 줄 서는 방법, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,5 +1,4 @@
 def solution(n, k):
-    # 리스트 만들기
     arr = [i for i in range(1, n+1)]
     answer = []
     
@@ -13,28 +12,14 @@ def solution(n, k):
                 break
 
     return answer
-        # k가 (n-1)! * i 이하면 첫 번째 자리는 arr[i-1]이다
-        # 따라서 
-        
-        # n=4인 경우
-        # [1, 2, 3, 4]
-        # [1, 2, 4, 3]
-        # [1, 3, 2, 4]
-        # [1, 3, 4, 2]
-        # [1, 4, 2, 3]
-        # [1, 4, 3, 2]
-        # [2, 1, 3, 4]
-        # k=7이라면?
-        # 첫 번째 자리는 2인거 ㅇㅋ
-        # 그 다음엔?
-        # 남은 애들 중에서 k-(n-1)!*(i-1)번째를 뽑는다
     
 def factorial(n):
     temp = 1
     for i in range(1, n+1):
         temp *= i
     return temp
-    
+
+## 시간 초과
 #from itertools import permutations
 #
 #def solution(n, k):
