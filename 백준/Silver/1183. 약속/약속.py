12480e06db54ccb9308b180e03d73b51d6f1497f
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-09-15 18:33:20 +0100

    [Silver II] Title: 약속, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1183. 약속/약속.py
@@ -0,0 +1,12 @@
+N = int(input())
+arr = []
+for i in range(N):
+    a, b = map(int, input().split())
+    arr.append(a-b)
+
+arr.sort()
+
+if N%2 == 1:
+    print(1)
+else:
+    print(abs(arr[N//2] - arr[N//2-1]) + 1)
