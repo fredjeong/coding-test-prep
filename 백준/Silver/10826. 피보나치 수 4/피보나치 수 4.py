2d5a4ed45bdcf100546e985176503d38c5c24edc
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-09-23 06:32:16 +0800

    [Silver V] Title: 피보나치 수 4, Time: 60 ms, Memory: 34912 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+if N==0:
+    print(0)
+elif N==1:
+    print(1)
+else:
+    dp = [0 for _ in range(N+1)]
+    dp[1] = 1
+    for i in range(2, N+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    print(dp[-1])
