575e3c4fc016ae88fdad975ef2222730fc542baf
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-09-24 11:22:45 +0800

    [Silver III] Title: 이친수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+if N==1:
+    print(1)
+else:
+    dp = [0 for _ in range(N+1)]
+    dp[1] = 1
+    for i in range(2, N+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    print(dp[-1])
