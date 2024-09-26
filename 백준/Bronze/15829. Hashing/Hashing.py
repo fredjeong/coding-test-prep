f9494edaae4dff06ec81bc6f3a4f82046118751a
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-09-27 06:57:45 +0800

    [Bronze II] Title: Hashing, Time: 32 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+string = input().strip()
+r = 31
+M = 123456791
+
+dic = {}
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+for i in range(len(alphabet)):
+    dic[alphabet[i]] = i+1
+
+total = 0
+for i in range(len(string)):
+    total += dic[string[i]]*(r**i) % M
+
+print(total)
