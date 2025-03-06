ab3df1bf0e67fd2045e5f4c8458fdc60d1d9ef5a
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2025-03-06 16:50:58 +0900

    [D6] Title: 영준이의 진짜 BFS, Time: 9,504 ms, Memory: 119,540 KB -BaekjoonHub

---
+++ SWEA/D6/1855. 영준이의 진짜 BFS/영준이의 진짜 BFS.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+T = int(input())
+
+def lca(node1, node2):
+    global answer
+    while depth[node1] > depth[node2]:
+        answer += 1
+        node1 = child_parent[node1]
+    while depth[node1] < depth[node2]:
+        answer += 1
+        node2 = child_parent[node2]
+    while node1 != node2:
+        answer += 2
+        node1 = child_parent[node1]
+        node2 = child_parent[node2]
+
+for test_case in range(1, T+1):
+
+    answer = 0
+    N = int(input())
+    input_list = list(map(int, input().split()))
+
+    i = 2
+
+    parent_child = [[] for _ in range(N + 1)]
+    child_parent = [0] * (N + 1)
+    depth = [0] * (N + 1)
+
+    depth[1] = 1
+    for parent in input_list:
+        parent_child[parent].append(i)
+        child_parent[i] = parent
+        depth[i] = depth[parent] + 1
+        i += 1
+
+    queue = deque([1])
+
+    cur_node = 1
+    while queue:
+        search_node = queue.popleft()
+
+        for child in parent_child[search_node]:
+            queue.append(child)
+            lca(cur_node, child)
+            cur_node = child
+
+    print(f"#{test_case} {answer}")
