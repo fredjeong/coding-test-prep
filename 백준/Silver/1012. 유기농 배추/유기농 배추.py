ed84acb6b821472cafc88f45c5cfbe8881e6a2c1
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2025-04-03 15:58:22 +0900

    [Silver II] Title: 유기농 배추, Time: 56 ms, Memory: 31420 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,34 +1,28 @@
-T = int(input())
-
-from collections import deque
-
-def solution(M, N, graph):
-    visited = [False for _ in range(len(graph))]
-    visited[0] = True
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
-    q = deque()
-    answer = 0
-    while graph:
-        x, y = graph.pop()
-        q.append([x, y])
-        answer += 1
-        while q: 
-            x, y = q.popleft()
-            for j in range(len(dx)):
-                nx = x + dx[j]
-                ny = y + dy[j]
-                if [nx, ny] in graph:
-                    idx = graph.index([nx, ny])
-                    graph.pop(idx)
-                    q.append([nx, ny])
-    return answer
-
-clear = 0
-while clear < T:
-    M, N, K = map(int, input().split())
-    graph = [list(map(int, input().split())) for _ in range(K)]
-    if __name__ == '__main__':
-        result = solution(M, N, graph)
-        print(result)
-    clear += 1
+import sys
+sys.setrecursionlimit(10**8)
+#dfs
+def dfs(x,y):
+    if x<= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if board[x][y] == 1:
+        board[x][y] = 0
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x,y-1)
+        dfs(x,y+1)
+        return True
+    return False
+    
+t = int(input())
+for i in range(t):
+    m,n,k = map(int, sys.stdin.readline().split())
+    board = [[0]*(m) for h in range(n)]
+    for j in range(k):
+        u_y,v_x = map(int, sys.stdin.readline().split())
+        board[v_x][u_y] = 1 
+    cnt = 0
+    for a in range(n):
+        for b in range(m):
+            if dfs(a,b) == True:
+                cnt += 1
+    print(cnt)
